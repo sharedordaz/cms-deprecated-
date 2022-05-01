4db5f5496a22ded9a811f059cbb1a7a287ffc3ee
sharedordaz <sharedordaz@live.com.mx>
2022-04-30 23:56:37 -0500

    added components

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header } from './header.component.ts/header.component.ts.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsListComponent } from './contacts/contacts-list/contacts-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header.Component.TsComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactsListComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
