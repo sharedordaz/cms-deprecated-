a182d6e3b4820672f356a84efac093872ae27f69
sharedordaz <sharedordaz@live.com.mx>
2022-05-01 01:39:32 -0500

    commit

--- cms/src/app/contacts/contacts-list/contacts-list.component.ts
+++ cms/src/app/contacts/contacts-list/contacts-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  contacts: [] = 
+  contacts: [] = [];
 
   constructor() { }
 
